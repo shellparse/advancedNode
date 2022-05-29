2db978a604386a023e30dd08bb544522abc09abc
greekmido <onex911@hotmail.com>
2022-05-29 22:53:07 +0200

    any

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session =require("express-session");
 const passport = require("passport");
+const ObjectID = require('mongodb').ObjectID;
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -21,7 +22,15 @@ app.route('/').get((req, res) => {
 app.use(session({secret:process.env.SESSION_SECRET,resave:true,saveUninitialized:true,cookie:{secure:false}}));
 app.use(passport.initialize());
 app.use(passport.session());
-
+//#3 serialization of users using the user id from the database
+passport.serializeUser((user,done)=>{
+  done(null,user._id);
+})
+passport.deserializeUser((id,done)=>{
+  done(null,null)
+  // myDataBase.findOne({_id:new ObjectID(id)},(err,doc)=>{
+  // })
+})
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
