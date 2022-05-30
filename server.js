ee260a550701b4639b9652eb6a1f0adc5029ff23
mohammad alameri <onex911@hotmail.com>
2022-05-30 08:23:24 +0200

    using passport local strategy

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session =require("express-session");
 const passport = require("passport");
 const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -31,6 +32,18 @@ passport.deserializeUser((id,done)=>{
   // myDataBase.findOne({_id:new ObjectID(id)},(err,doc)=>{
   // })
 })
+//#4 using passport middleware passport-local
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    myDataBase.findOne({ username: username }, function (err, user) {
+      console.log('User '+ username +' attempted to log in.');
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (password !== user.password) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
