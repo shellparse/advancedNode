3731e9dd7605b0e6cb4fc828037f4b55f00e6e7f
mohammad alameri <onex911@hotmail.com>
2022-06-13 16:35:37 +0200

    connected clients counter

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const passport = require("passport");
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const passportSocketIo=require("passport.socketio");
+const MongoStore=require("connect-mongo")(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
+const cookieParser=require("cookie-parser");
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
